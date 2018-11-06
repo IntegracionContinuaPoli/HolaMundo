5bdbed4c54b7436a842f2b809c9049bebb122b24
IntegracionContinuaPoli <44612028+IntegracionContinuaPoli@users.noreply.github.com>
2018-11-05 21:22:15 -0500

    Add files via upload

---
+++ HolaMundo.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+	
+		<title>Integracion Continua</title>
+		
+		<style type="text/css">
+		
+			.message
+			{
+				border: solid 1px #000000;
+				margin: 15% auto;
+				width:20%;
+				height: 20%;
+				color: #000000;
+			}
+		
+		
+		</style>
+		
+	</head>
+	<body>
+		
+		<p class="message">
+		
+			<?php echo 'Hola Mundo'; ?>
+			
+		</p>
+	
+	</body>
+</html>
